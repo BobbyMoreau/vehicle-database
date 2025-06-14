c36b682824fd237e344bd87c885a8106fbcdfd2e
BobbyMoreau <bobbymoreauraquin@gmail.com>
2025-06-14 11:51:43 +0200

    simple get

--- Backend/Controllers/VehiclesController.cs
+++ Backend/Controllers/VehiclesController.cs
@@ -1,11 +1,32 @@
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace MyApp.Namespace
+
+namespace Vehiclesdatabase.api.Namespace
 {
     [Route("api/[controller]")]
     [ApiController]
     public class VehiclesController : ControllerBase
     {
+        private readonly VehicleDbContext _context;
+
+        public VehiclesController(VehicleDbContext  context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vehicle>>> GetVehicles()
+        {
+            var vehicles = await _context.Vehicles
+                .Include(v => v.Brand)
+                .Include(v => v.VehicleEquipments)
+                    .ThenInclude(ve => ve.Equipment)
+                .ToListAsync();
+
+            return Ok(vehicles);
+        }
     }
+
+    
 }
